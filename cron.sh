682b80ef9654b96c6a151ea5b8f71136f3e6ce03
kuwacom <kuwa.com3@gmail.com>
2025-04-20 05:58:51 +0900

    update: cron jobを更新
    再構成時に自動で update:htaccess を実行するように変更

--- cron.sh
+++ cron.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+sleep 30 # wait for Nextcloud to start
+
+# docker compose up で再生成したときに実行
+php /var/www/html/occ maintenance:update:htaccess
+
 while true; do
+    echo "Running cron job at $(date)"
     php -f /var/www/html/cron.php
     sleep 300 # 5分ごとに実行
 done
