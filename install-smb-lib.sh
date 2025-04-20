0f628f596cdc85f6cf72630a6cee7340541aad04
kuwacom <kuwa.com3@gmail.com>
2025-04-21 01:51:10 +0900

    update: SMB互換接続パッケージの追加

--- install-smb-lib.sh
+++ install-smb-lib.sh
@@ -1,1 +1,1 @@
-docker exec -it nextcloud_app bash -c "apt-get update && apt-get install -y libsmbclient-dev smbclient && pecl install smbclient && docker-php-ext-enable smbclient"
+docker exec -it nextcloud_app bash -c "apt-get update && apt-get install -y libsmbclient-dev smbclient && pecl install smbclient cifs-utils && docker-php-ext-enable smbclient"
